cf2885aafc5279387a262dcac0e1fbe1f52108c7
armandossrecife <armando.sousa@gmail.com>
2023-07-21 16:36:22 -0300

    Refatoracao dos casos de testes usuarios e chamada de suite de testes.

--- app/tests/test_run.py
+++ app/tests/test_run.py
@@ -1,12 +1,10 @@
 import unittest
-from app.repository import UsuarioRepository
 from app.tests.unit_tests.CT_TU_01 import TestUsuarioRepositoryMethods
 
 class TestRun():
     def __init__(self):
-        self.usuario_repository = UsuarioRepository(db='usuarios_test.db')
         self.suite = unittest.TestSuite()
-        self.suite.addTest(TestUsuarioRepositoryMethods(usuario_repository=self.usuario_repository))
+        self.suite.addTest(TestUsuarioRepositoryMethods())
 
     def run(self):
         runner = unittest.TextTestRunner()
--- app/tests/unit_tests/CT_TU_01.py
+++ app/tests/unit_tests/CT_TU_01.py
@@ -3,29 +3,45 @@ from app.models import Usuario
 from app.repository import UsuarioRepository
 
 class TestUsuarioRepositoryMethods(unittest.TestCase):
-    def setUp(self, usuario_repository=UsuarioRepository()):
-        self.usuario_repository: UsuarioRepository = usuario_repository
-        self.usuario_repository.c.execute("DELETE FROM usuarios")
-        self.usuario_repository.conn.commit()
-    
-    def test_verificar_se_tabela_existe(self):
-        self.usuario_repository.c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='usuarios'")
-        self.assertTrue(self.usuario_repository.c.fetchone())
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.usuario_repository = UsuarioRepository(db='usuarios_test.db')
+
+    def free_user_data(self):
+        self.usuario_repository.remover_usuarios()
+        self.usuario_repository.conn.close()
+
+    def procura_em_lista_tuplas(self, string_procurada, lista_de_tuplas):
+        # Verifica se a string está presente em alguma posição de alguma tupla da lista
+        if any(string_procurada in tupla for tupla in lista_de_tuplas):
+            resultado = True
+        else:
+            resultado = False
+        return resultado
+
+    def setUp(self):
+        self.usuario_repository.remover_usuarios()
+            
     def test_cadastrar_usuario(self):
-        usuario = Usuario('teste', 'jose@gmail.com', '123')
+        usuario = Usuario(id=None, fullname='Teste Jose', email='jose@gmail.com',username='testexpto', password='123')
         self.usuario_repository.cadastrar_usuario(usuario)
-        self.usuario_repository.c.execute("SELECT * FROM usuarios WHERE nome=?", (usuario.nome,))
-        self.assertEqual(self.usuario_repository.c.fetchone(), ('teste', 'jose@gmail.com', '123'))
+        retorno = self.usuario_repository.buscar_usuario_por_username(usuario.username)
+        id, nome_completo, e_mail, username, senha = retorno[0], retorno[1], retorno[2], retorno[3], retorno[4]
+        self.assertEqual(nome_completo, 'Teste Jose')
+        self.assertEqual(e_mail, 'jose@gmail.com')
+        self.assertEqual(username, 'testexpto')
+        self.assertEqual(senha, '123')
 
     def test_listar_usuarios(self):
-        usuario1 = Usuario('teste1', 'joao@gmail.com', '123')
-        usuario2 = Usuario('teste2', 'josias@outlook.com', '123')
+        usuario1 = Usuario(id=None, fullname='Teste1 Joao', email='joao@gmail.com', username='teste1', password='123')
+        usuario2 = Usuario(id=None, fullname='Teste2 Josias', email='josias@outlook.com', username='teste2',password='123')
         self.usuario_repository.cadastrar_usuario(usuario1)
         self.usuario_repository.cadastrar_usuario(usuario2)
-        self.assertEqual(self.usuario_repository.listar_usuarios(), [('teste1', 'joao@gmail.com', '123'), ('teste2', 'josias@outlook.com', '123')])
+        qtd_usuarios = len(self.usuario_repository.listar_usuarios())
+        self.assertEqual(qtd_usuarios, 2)
+        # Pega usuario, faz a busca na lista e checa se aparece na lista
+        self.assertTrue(self.procura_em_lista_tuplas(usuario1.username,self.usuario_repository.listar_usuarios()))
+        self.assertTrue(self.procura_em_lista_tuplas(usuario2.username,self.usuario_repository.listar_usuarios()))
     
-    def test_buscar_usuario(self):
-        usuario = Usuario('teste', 'jose@gmail.com', '123')
-        self.usuario_repository.cadastrar_usuario(usuario)
-        self.assertEqual(self.usuario_repository.buscar_usuario_por_username('teste'), ('teste', 'jose@gmail.com', '123'))
+    def tearDown(self):
+        self.free_user_data()
